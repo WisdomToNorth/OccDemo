d4f74cd529e9c92f0caeb229df9249120954ed19
kangyucheng <kangyucheng1998@126.com>
2023-05-15 10:57:33 +0800

    Some edit

--- src/KTimer.h
+++ src/KTimer.h
@@ -11,33 +11,42 @@ public:
     KTimer() :
         t1_(std::chrono::steady_clock::now()), t2_(t1_)
     {}
-
+    template <typename T = std::milli>
     double timeFromBegin(const std::string &str = "\nTime from start: ")
     {
-        double res = timeFromBeginP();
-        std::cout << str << res << " ms" << std::endl;
+        double res = timeFromBeginP<T>();
+        if (!str.empty())
+        {
+            std::cout << str << res << " ms" << std::endl;
+        }
         return res;
     }
-
+    template <typename T = std::milli>
     double timeFromLastSee(const std::string &str = "\nTime from last see: ")
     {
-        double dr_ms = timeFromLastSeeP();
-        std::cout << str << dr_ms << " ms" << std::endl;
+        double dr_ms = timeFromLastSeeP<T>();
+        if (!str.empty())
+        {
+            std::cout << str << dr_ms << " ms" << std::endl;
+        }
         return dr_ms;
     }
 
 private:
+    template <typename T>
     double timeFromBeginP()
     {
         t2_ = std::chrono::steady_clock::now();
-        double dr_ms = std::chrono::duration<double, std::milli>(t2_ - t1_).count();
+        double dr_ms = std::chrono::duration<double, T>(t2_ - t1_).count();
 
         return dr_ms;
     }
+
+    template <typename T>
     double timeFromLastSeeP()
     {
         auto t3 = std::chrono::steady_clock::now();
-        double dr_ms = std::chrono::duration<double, std::milli>(t3 - t2_).count();
+        double dr_ms = std::chrono::duration<double, T>(t3 - t2_).count();
         t2_ = t3;
 
         return dr_ms;
